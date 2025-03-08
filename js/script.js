d0146f5139b57270302fa49cb37a79812893166e
prasath6379 <pp31804@gmail.com>
2025-03-08 13:59:14 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let icon = document.querySelector(".icon");
+let nav = document.querySelector("ul"); // Select the whole navigation list
+
+
+icon.addEventListener("click", () => {
+        nav.classList.toggle("showmenu");
+    });
